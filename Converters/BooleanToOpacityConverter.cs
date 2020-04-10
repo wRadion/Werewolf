c7ee60d4ae40e4363d925e87fb7de6130bee534b
wRadion <wradion@gmail.com>
2020-04-10 15:08:46 +0200

    [Views]: MainWindow done!

---
+++ Converters/BooleanToOpacityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Werewolf.Converters
+{
+    public class BooleanToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b = System.Convert.ToBoolean(value);
+
+            return b ? 1.0d : 0.5d;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
